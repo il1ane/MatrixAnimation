bf671647e96c14c0293aeb7caacbacc1934b41e9
il1ane <iliane.zedadra@icloud.com>
2021-06-25 21:35:28 +0200

    Initial Commit

---
+++ MatrixAnimation/MatrixAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatrixAnimationApp.swift
+//  MatrixAnimation
+//
+//  Created by Iliane Zedadra on 25/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct MatrixAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
